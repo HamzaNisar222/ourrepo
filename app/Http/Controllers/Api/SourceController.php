ccba0ba17259ea12271bea52bc5100572640f00a
HamzaNisar222 <hamzanisar941@gmail.com>
2024-08-01 16:11:34 +0500

    merge

--- app/Http/Controllers/Api/SourceController.php
+++ app/Http/Controllers/Api/SourceController.php
@@ -1,25 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
-use Illuminate\Http\Request;
-use App\Models\AvailableSource;
-use App\Models\AvailableResource;
 use App\Http\Controllers\Controller;
 
 
 class SourceController extends Controller
 {
-    // /**
-    //  * Store a newly created available source in storage.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @return \Illuminate\Http\Response
-    //  */
-    // public function createAvailableSource(Request $request)
-    // {
-    //    // Use the model method to create the available source
-    //    $availableSource = AvailableSource::createSource($request);
-    //    return response()->json(['message' => 'Available source created successfully', 'data' => $availableSource], 201);
-    // }
 }
